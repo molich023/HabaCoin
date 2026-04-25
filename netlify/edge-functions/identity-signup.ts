63a8dcd790acbd00b83a29df762cb81d69a804bc
Molich <molich60@gmail.com>
2026-04-25 11:46:17 +0300

    Update import path for Context in identity-signup.ts

--- netlify/edge-functions/identity-signup.ts
+++ netlify/edge-functions/identity-signup.ts
@@ -1,6 +1,8 @@
-import { Context } from "https://edge.netlify.com";
+import { Context } from "@netlify/edge-functions";
 
+// The "Default Export" is mandatory for Edge Functions
 export default async (request: Request, context: Context) => {
+  try {
   const { user } = await request.json();
   
   // Netlify automatically provides the country of the user in the 'geo' object
