42b375135ed9cf49d0d076aa72cffb9a72ee0491
Molich <molich60@gmail.com>
2026-06-13 23:40:08 +0300

    Delete src/lib directory

--- src/lib/neon.ts
+++
@@ -1,7 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-
-// This function works in Next.js Edge Functions and Server Actions
-export const getDb = () => {
-  const sql = neon(process.env.DATABASE_URL!);
-  return sql;
-};
